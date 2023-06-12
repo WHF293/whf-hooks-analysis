200e17809350745b475196930b80df50b4f0d729
hfWang <2860028714@qq.com>
2023-06-12 14:23:44 +0800

    feat

--- hooks/.vitepress/config.ts
+++ hooks/.vitepress/config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     lastUpdatedText: '最后更新时间',
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2023-present HfWang'
+      copyright: 'Copyright © 2023-present wanghaofeng'
     },
     editLink: {
       pattern: 'https://github.com/WHF293/whf-hooks-analysis/tree/master/hooks/:path',
